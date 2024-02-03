eaa6c67bf62d1277e3edfc88b3865a08e8bb834b
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-03 18:01:31 +0300

    Complete OAuth ui and installed firebase

--- src/Components/OAuth.tsx
+++ src/Components/OAuth.tsx
@@ -1,11 +1,39 @@
+import { BsGithub } from "react-icons/bs";
 import { GrGoogle } from "react-icons/gr";
 
 const OAuth = () => {
+  const handleGithubAuthClick = () => {
+    // Implement OAuth logic here
+    console.log("OAuth button of Github got clicked");
+  };
+
+  const handleGoogleAuthClick = () => {
+    // Implement OAuth logic here
+    console.log("OAuth button of Google got clicked");
+  };
+
   return (
-    <button className="flex items-center gap-3 bg-red-500 p-3 text-white rounded-lg">
-      <GrGoogle />
-      Continue with Google
-    </button>
+    <div className="flex flex-col gap-5 mt-20">
+      <button
+        onClick={handleGoogleAuthClick}
+        className="flex items-center gap-3 bg-red-500 p-3 text-white rounded-lg"
+      >
+        <GrGoogle />
+        Continue with Google
+      </button>
+      <div className="flex items-center gap-4">
+        <div className="flex-grow h-0.5 bg-gray-300"></div>
+        <p className="text-gray-500">or</p>
+        <div className="flex-grow h-0.5 bg-gray-300"></div>
+      </div>
+      <button
+        onClick={handleGithubAuthClick}
+        className="flex items-center gap-3 bg-gray-900 p-3 text-white rounded-lg "
+      >
+        <BsGithub />
+        Continue with Github
+      </button>
+    </div>
   );
 };
 
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -3,9 +3,7 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center flex-col">
       <h1 className="mt-32 font-semibold text-3xl">Login to your account</h1>
-      <div className="mt-28">
-        <OAuth />
-      </div>
+      <OAuth />
     </div>
   );
 };
